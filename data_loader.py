aecf213eb9c9f6bdbfc36e65d0b18ede64ae5903
Francisco Rubin Capalbo <pancho_111203@hotmail.com>
2018-03-01 16:34:04 +0100

    added visualization of candles

--- data_loader.py
+++ data_loader.py
@@ -8,6 +8,7 @@ from dateutil.parser import parse as dateparse
 import gdax
 import math
 import json
+import pandas as pd
 
 public_client = gdax.PublicClient()
 
@@ -23,64 +24,68 @@ def getCandles(coinPair, granularity, start, end, save=True):
         logger.debug('Loading data from file: {}'.format(filename))
         with open(filename, 'r') as f:
             allCandles = json.load(f)
-        return allCandles
-
-    def requestCandlesAndSave(startTime_, endTime_):
-        logger.debug('Getting data from {} to {}'.format(startTime_.isoformat(), endTime_.isoformat()))
-        for attempt in range(0, 10):   
-            if attempt > 0:
-                logger.debug('Attempt {}'.format(attempt+1))
-            candles = public_client.get_product_historic_rates(coinPair, granularity=granularity, start=startTime_.isoformat(), end=endTime_.isoformat())
-            if isinstance(candles, list):
-                candles.reverse()
-                return candles
-            else:
-                logger.error('ERROR requesting candles: {}'.format(candles))
-                logger.error('Trying again')
+        
+    else:
+        def requestCandlesAndSave(startTime_, endTime_):
+            logger.debug('Getting data from {} to {}'.format(startTime_.isoformat(), endTime_.isoformat()))
+            for attempt in range(0, 10):   
+                if attempt > 0:
+                    logger.debug('Attempt {}'.format(attempt+1))
+                candles = public_client.get_product_historic_rates(coinPair, granularity=granularity, start=startTime_.isoformat(), end=endTime_.isoformat())
+                if isinstance(candles, list):
+                    candles.reverse()
+                    return candles
+                else:
+                    logger.error('ERROR requesting candles: {}'.format(candles))
+                    logger.error('Trying again')
+                    
+            return []
+            
+        if granularity != 60 and granularity != 300 and granularity != 900 and granularity != 3600 and granularity != 21600 and granularity != 86400:
+            logger.error('Invalid granularity({}), must be one of the following values: {60, 300, 900, 3600, 21600, 86400}'.format(granularity))
+            return []
+        
+        startTime = dateparse(start)
+        endTime = dateparse(end)
+        
+        if endTime < startTime:
+            logger.error('Starting date ({}) is after ending date ({})'.format(start, end))
+            return []
+    
+        maxResultsPerCall = 350 # described in https://docs.gdax.com/#get-historic-rates
+        secondsCoveredPerCall = maxResultsPerCall * granularity
+        
+        expectedResultsLength = int((endTime - startTime).total_seconds() / granularity) + 1
+        numberOfRequests = math.ceil(expectedResultsLength / maxResultsPerCall)
+        logger.debug('expectedResultsLength: {}'.format(expectedResultsLength))
+        logger.debug('numberOfRequests: {}'.format(numberOfRequests))
+        
+        allCandles = []
+        currentStartTime = startTime
+        for i in range(0, numberOfRequests - 1):
+            currentEndTime = currentStartTime + datetime.timedelta(seconds=secondsCoveredPerCall)
+    
+            allCandles += requestCandlesAndSave(currentStartTime, currentEndTime)
                 
-        return []
+            currentStartTime = currentEndTime + datetime.timedelta(seconds=granularity)
             
-    if granularity != 60 and granularity != 300 and granularity != 900 and granularity != 3600 and granularity != 21600 and granularity != 86400:
-        logger.error('Invalid granularity({}), must be one of the following values: {60, 300, 900, 3600, 21600, 86400}'.format(granularity))
-        return []
-    
-    startTime = dateparse(start)
-    endTime = dateparse(end)
-    
-    if endTime < startTime:
-        logger.error('Starting date ({}) is after ending date ({})'.format(start, end))
-        return []
-
-    maxResultsPerCall = 350 # described in https://docs.gdax.com/#get-historic-rates
-    secondsCoveredPerCall = maxResultsPerCall * granularity
-    
-    expectedResultsLength = int((endTime - startTime).total_seconds() / granularity) + 1
-    numberOfRequests = math.ceil(expectedResultsLength / maxResultsPerCall)
-    logger.debug('expectedResultsLength: {}'.format(expectedResultsLength))
-    logger.debug('numberOfRequests: {}'.format(numberOfRequests))
-    
-    allCandles = []
-    currentStartTime = startTime
-    for i in range(0, numberOfRequests - 1):
-        currentEndTime = currentStartTime + datetime.timedelta(seconds=secondsCoveredPerCall)
-
-        allCandles += requestCandlesAndSave(currentStartTime, currentEndTime)
-            
-        currentStartTime = currentEndTime + datetime.timedelta(seconds=granularity)
+        # last request only takes until endTime
+        allCandles += requestCandlesAndSave(currentStartTime, endTime)
         
-    # last request only takes until endTime
-    allCandles += requestCandlesAndSave(currentStartTime, endTime)
+        if save:
+            if not(os.path.isdir("data")):
+                os.system("mkdir data; cd data; mkdir candles")
     
-    if save:
-        if not(os.path.isdir("data")):
-            os.system("mkdir data; cd data; mkdir candles")
+            logger.debug('Saving data on file: {}'.format(filename))
+            with open(filename, 'a+') as f:
+                json.dump(allCandles, f)
 
-        logger.debug('Saving data on file: {}'.format(filename))
-        with open(filename, 'a+') as f:
-            json.dump(allCandles, f)
+    candlesFrame = pd.DataFrame(allCandles)
+    candlesFrame.index = pd.to_datetime(candlesFrame[0], unit='s')
+    del candlesFrame[0]
+    candlesFrame.columns = ['low', 'high', 'open', 'close', 'volume' ]
 
-        
-    return allCandles
+    return candlesFrame
 
 
 if __name__ == '__main__':  
@@ -88,5 +93,3 @@ if __name__ == '__main__':
     if len(logger.handlers) == 0:
         logger.addHandler(logging.StreamHandler())
     candles = getCandles('ETH-USD', granularity=60, start='2018-02-27T12:50:25+01:00', end='2018-02-28T12:50:25+01:00', save=True)
-    print('Results: {}'.format(len(candles)))
-    print('First time: {}, Last time: {}'.format(datetime.datetime.fromtimestamp(candles[0][0]), datetime.datetime.fromtimestamp(candles[-1][0])))
---
+++ visualization.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import json
+import pandas as pd
+import os
+import plotly.offline as py
+import plotly.graph_objs as go
+import data_loader
+
+candles = data_loader.getCandles('ETH-USD', 60, '2018-02-25T00:00:25+01:00', '2018-02-25T23:58:25+01:00')
+#1 hr moving average
+movingAverage1hr = candles.close.rolling(60, min_periods=60).mean()
+
+candlesChart = go.Candlestick(x=candles.index,
+                       open=candles.open,
+                       high=candles.high,
+                       low=candles.low,
+                       close=candles.close)
+
+opens = go.Scatter(
+    x=candles.index,
+    y=candles.open,
+    name='open'
+)
+
+movingAverage1hrPlot = go.Scatter(
+    x=movingAverage1hr.index,
+    y=movingAverage1hr.values,
+    name='movingAverage1hr'
+)
+    
+data = [candlesChart, opens, movingAverage1hrPlot]
+fig = go.Figure(data=data)
+py.plot(fig, filename=os.path.join(os.path.dirname(__file__), 'plots/eth_usd_candles.html'))
