d2e1ae58e7d0f4cd7bf65fb77ca3537667b74c86
Francisco Rubin Capalbo <pancho_111203@hotmail.com>
2018-05-15 15:52:49 +0200

    fixed simple_cnn

--- prediction/tendency.simple_cnn.py
+++ prediction/tendency.simple_cnn.py
@@ -31,7 +31,8 @@ print('Starting...')
 #X = random.sample(range(0, 1000), 1000)
 #y = [X[k-2]+2 for (k, v) in enumerate(X)]
 
-metrics = Metrics(os.path.join(os.path.dirname(__file__), '../logs/simple_cnn.json'))
+metrics = Metrics(os.path.join(os.path.dirname(
+    __file__), '../logs/simple_cnn.json'))
 
 if not os.path.isdir(os.path.join(os.path.dirname(__file__), 'checkpoint/{}'.format(checkpoint_name))):
     dire = os.path.join(os.path.dirname(__file__), 'checkpoint/')
@@ -39,16 +40,19 @@ if not os.path.isdir(os.path.join(os.path.dirname(__file__), 'checkpoint/{}'.for
 
 parser = argparse.ArgumentParser(description='LSTM Crypto Prediction')
 parser.add_argument('--lr', default=0.0001, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true', default=False, help='resume from checkpoint')
-parser.add_argument('--visualize_results', '-v', action='store_true', default=False, help='use this to visualize the results of the currently trained model')
+parser.add_argument('--resume', '-r', action='store_true',
+                    default=False, help='resume from checkpoint')
+parser.add_argument('--visualize_results', '-v', action='store_true', default=False,
+                    help='use this to visualize the results of the currently trained model')
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 best_loss = 99999
 
-###Load data
-dataset = data_loader.getCandles('ETH-USD', 60, start='2018-02-01T00:00:25+01:00', end='2018-05-01T00:00:25+01:00', save=True)
+# Load data
+dataset = data_loader.getCandles(
+    'ETH-USD', 60, start='2018-02-01T00:00:25+01:00', end='2018-05-01T00:00:25+01:00', save=True)
 #dataset = data_loader.getCandles('ETH-USD', 60, start='2016-10-14T00:00:25+01:00', end='2018-03-22T00:00:25+01:00', save=True)
 #dataset = data_loader.getCandles('ETH-USD', 60, start='2018-02-14T00:00:25+01:00', end='2018-03-14T00:00:25+01:00', save=True)
 
@@ -64,7 +68,8 @@ addTendency(dataset, threshold=0.10)
 # split into train and test sets
 train_size = int(len(dataset) * 0.7)
 test_size = len(dataset) - train_size
-train, test = dataset.iloc[0:train_size,:], dataset.iloc[train_size:len(dataset),:]
+train, test = dataset.iloc[0:train_size,
+                           :], dataset.iloc[train_size:len(dataset), :]
 
 # print(len(train), len(test))
 
@@ -76,8 +81,8 @@ testX = test[['open', 'volume']].values
 testY = test['tendency'].values
 
 x_features = 1 if len(trainX.shape) == 1 else trainX.shape[1]
-look_back=6000
-batch_size=100
+look_back = 6000
+batch_size = 100
 
 # using unsqueeze because of this: https://github.com/pytorch/pytorch/issues/2539
 trainX = torch.from_numpy(trainX).float()
@@ -86,23 +91,30 @@ testX = torch.from_numpy(testX).float()
 testY = torch.from_numpy(testY).float().unsqueeze(1)
 
 if use_cuda:
-    trainX, trainY, testX, testY = trainX.cuda(), trainY.cuda(), testX.cuda(), testY.cuda()
+    trainX, trainY, testX, testY = trainX.cuda(
+    ), trainY.cuda(), testX.cuda(), testY.cuda()
 
 train_dataset = TensorDataset(trainX, trainY)
 test_dataset = TensorDataset(testX, testY)
 
+
 def collate_fn(_, indices, all_data):
-    X = [all_data.tensors[0][index-look_back:index] for index in indices]
+    X = [all_data.tensors[0][index - look_back:index] for index in indices]
     y = [all_data.tensors[1][index] for index in indices]
     return (torch.stack(X), torch.stack(y))
 
 
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(list(range(look_back, len(train_dataset)))), collate_fn=collate_fn)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(list(range(look_back, len(test_dataset)))), collate_fn=collate_fn)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(
+    list(range(look_back, len(train_dataset)))), collate_fn=collate_fn)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(
+    list(range(look_back, len(test_dataset)))), collate_fn=collate_fn)
 
-checkpoint_path = os.path.join(os.path.dirname(__file__), 'checkpoint/{}/weights.ckpt'.format(checkpoint_name))
+checkpoint_path = os.path.join(os.path.dirname(
+    __file__), 'checkpoint/{}/weights.ckpt'.format(checkpoint_name))
 
 # Model
+
+
 class Model(nn.Module):
     def __init__(self, n_features, look_back):
         super(Model, self).__init__()
@@ -111,15 +123,34 @@ class Model(nn.Module):
 
         self.avg_pool = nn.AvgPool1d(2)
 
-        self.conv_0 = self.convlayer(5, 32)
-        self.conv_1 = self.convlayer(5, 64)
-        self.conv_2 = self.convlayer(5, 128)
-        self.conv_3 = self.convlayer(5, 256)
-        self.conv_4 = self.convlayer(5, 512)
+        self.conv_0 = nn.Sequential(
+            nn.Conv1d(2, 32, 5, stride=2),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+        self.conv_1 = nn.Sequential(
+            nn.Conv1d(32, 64, 5, stride=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv1d(64, 128, 5, stride=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+        self.conv_3 = nn.Sequential(
+            nn.Conv1d(128, 256, 5, stride=2),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
 
         self.drop1 = nn.Dropout(p=0.5)
 
-        self.fc1 = nn.Linear(93*512, 512)
+        self.fc1 = nn.Linear(2560, 512)
         self.bn1 = nn.BatchNorm1d(512)
 
         self.drop2 = nn.Dropout(p=0.5)
@@ -128,26 +159,8 @@ class Model(nn.Module):
 
         self.drop3 = nn.Dropout(p=0.5)
         self.fc3 = nn.Linear(256, 1)
-    
-    def convlayer(self, kernel_size, out_features=100, max_pool=True):
-        #CNN
-        #In:        (N, C(self.n_features), L(self.look_back))
-        #Out:       (N, Cout(out_features), Lout)
-        if max_pool:
-            return nn.Sequential(
-                nn.Conv1d(self.n_features, out_features, kernel_size, stride=2),
-                nn.BatchNorm1d(out_features),
-                nn.ReLU(),
-                nn.MaxPool1d(2)
-            )
-        else:
-            return nn.Sequential(
-                nn.Conv1d(self.n_features, out_features, kernel_size, stride=2),
-                nn.BatchNorm1d(out_features),
-                nn.ReLU()
-            )
 
-    #Standardize on the look_back dimension
+    # Standardize on the look_back dimension
     def standardize(self, x):
         mean = torch.mean(x, dim=1)
         std = torch.std(x, dim=1)
@@ -166,15 +179,14 @@ class Model(nn.Module):
         batch_size = x.shape[0]
 
         (out, mean, std) = self.standardize(x)
-
+        out = out.transpose(1, 2)
         out = self.avg_pool(out)
 
         out = self.conv_0(out)
         out = self.conv_1(out)
         out = self.conv_2(out)
         out = self.conv_3(out)
-        out = self.conv_4(out)
-        
+
         out = out.view(batch_size, -1)
         out = self.drop1(out)
         out = F.relu(self.bn1(self.fc1(out)))
@@ -186,6 +198,7 @@ class Model(nn.Module):
         out = F.sigmoid(self.fc3(out))
 
         return out
+
 
 if args.resume and os.path.isfile(checkpoint_path):
     # Load checkpoint.
@@ -201,10 +214,12 @@ else:
 
 if use_cuda:
     net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    net = torch.nn.DataParallel(
+        net, device_ids=range(torch.cuda.device_count()))
     cudnn.benchmark = True
 
-print('Model params: {}'.format(sum([param.nelement() for param in net.parameters()])))
+print('Model params: {}'.format(
+    sum([param.nelement() for param in net.parameters()])))
 
 criterion = nn.BCELoss()
 optimizer = optim.Adam(net.parameters(), lr=args.lr)
@@ -214,7 +229,7 @@ optimizer = optim.Adam(net.parameters(), lr=args.lr)
 def train(epoch):
     net.train()
     optimizer.zero_grad()
-    
+
     loss_total = 0
     for i, (X, y) in enumerate(train_dataloader):
         X, y = Variable(X), Variable(y)
@@ -228,6 +243,7 @@ def train(epoch):
     avg_loss = loss_total / len(train_dataloader)
     print('TRAIN: Avg Loss: {}'.format(avg_loss))
     return avg_loss
+
 
 def test(epoch, save=True):
     global best_loss
@@ -257,8 +273,9 @@ def test(epoch, save=True):
 
     return avg_loss
 
+
 if not args.visualize_results:
-    for epoch in range(start_epoch, start_epoch+3000):
+    for epoch in range(start_epoch, start_epoch + 3000):
         print('\nEpoch: %d' % epoch)
         start = time.time()
         train_loss = train(epoch)
@@ -266,14 +283,15 @@ if not args.visualize_results:
         end = time.time()
         timeTaken = end - start
         print('Epoch {} took {} seconds'.format(epoch, timeTaken))
-        metrics.track({'epoch': epoch, 'timeTaken': timeTaken, 'train_loss': train_loss, 'test_loss': test_loss })
+        metrics.track({'epoch': epoch, 'timeTaken': timeTaken,
+                       'train_loss': train_loss, 'test_loss': test_loss})
 
     metrics.save()
 else:
     pass
     # import plotly.offline as py
     # import plotly.graph_objs as go
-    
+
     # train_values = train(start_epoch).data.numpy()
     # test_values = test(start_epoch, save=False).data.numpy()
 
@@ -304,5 +322,3 @@ else:
     # data = [train_real_chart, train_predicted_chart, test_real_chart, test_predicted_chart]
     # fig = go.Figure(data=data)
     # py.plot(fig, filename=os.path.join(os.path.dirname(__file__), '../plots/prediction.html'))
-
-    
